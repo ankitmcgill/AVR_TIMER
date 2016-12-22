becf53f773866c76e5e9a56d7e97dafda975ac53
Ankit Bhatnagar <ankit.bhatnagarindia@gmail.com>
2016-12-22 14:10:33 +0530

    added basic library files

---
+++ AVR_TIMER.h
@@ -0,0 +1,19 @@
+//////////////////////////////////////////////
+// AVR TIMER LIBRARY
+//
+//
+// DECEMBER 22, 2016
+//
+// ANKIT BHATNAGAR
+// ANKIT.BHATNAGARINDIA@GMAIL.COM
+//////////////////////////////////////////////
+
+
+#ifndef _AVR_TIMER_H_
+#define _AVR_TIMER_H_
+
+#include <stdio.h>
+#include <avr/io.h>
+
+
+#endif
