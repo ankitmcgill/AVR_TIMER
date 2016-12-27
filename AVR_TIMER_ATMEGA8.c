900879088cfa1ac99ab7c8c0ca977143300e67db
Ankit Bhatnagar <ankit.bhatnagarindia@gmail.com>
2016-12-27 16:19:24 +0530

    added timer code for Atmega8

--- AVR_TIMER_ATMEGA8.c
+++ AVR_TIMER_ATMEGA8.c
@@ -23,6 +23,11 @@
 // *TIMER2 SAME AS TIMER0 EXCEPT IT SUPPORTS MORE
 // CLOCK PRESCALING OPTIONS
 //
+// MODES OF OPERATION
+//	1. TIMER 0 - 8 BIT - OVF
+//	2. TIMER 1 - 16 BIT - OVF, OC-A, OC-B [CTC MODE ON OC-A]
+//	3. TIMER 2 - 8 BIT - OVF, OC-A [CTC MODE ON OC-A]
+//
 // ONLY SUPPORTS THE FOLLOWING CLOCK SOURCES:
 //	1. INTERNAL (FROM IO CLOCK)
 //
@@ -47,3 +52,252 @@
 ///////////////////////////////////////////////////////
 
 #include "AVR_TIMER_ATMEGA8.h"
+
+void AVR_TIMER_Enable_Mode_Normal(uint8_t timer_num, uint8_t timer_clock, uint8_t interrupt_enable)
+{
+	//INITIALIZE AND START THE TIMER IN NORMAL MODE WITH THE SPECIFIED
+	//CLOCK. ENABLE INTERRUPT IF REQUESTED
+	//
+	//NORMAL MODE:
+	//	BOTTOM = 0x00
+	//	TOP = MAX = 0xFF
+	//	INTERRUPT = OVERFLOW (IF ENABLED)
+	
+	switch(timer_num)
+	{
+		case AVR_TIMER_8BIT_TIMER0:
+			//CLEAR COUNT AND SET CLOCK
+			TCNT0 = 0x00;
+			if(interrupt_enable == AVR_TIMER_INTERRUPT_ON)
+			{
+				TIMSK |= (1 << TOIE0);
+			}
+			//APPLY CLOCK. START THE TIMER
+			TCCR0 |= timer_clock;
+			break;
+
+		case AVR_TIMER_8BIT_TIMER2:
+			//CLEAR MODE AND SET NORMAL MODE
+			TCCR2 &= ~((1 << WGM21) | (1 << WGM20));
+			TCNT2 = 0x00;
+			if(interrupt_enable == AVR_TIMER_INTERRUPT_ON)
+			{
+				TIMSK |= (1 << TOIE2);
+			}
+			//APPLY CLOCK. START THE TIMER
+			TCCR2 |= timer_clock;
+			break;
+
+		case AVR_TIMER_16BIT_TIMER1:
+			//CLEAR MODE AND SET NORMAL MODE
+			TCCR1A &= ~((1 << WGM11) | (1 << WGM10));
+			TCNT1 = 0x0000;
+			if(interrupt_enable == AVR_TIMER_INTERRUPT_ON)
+			{
+				TIMSK |= (1 << TOIE1);
+			}
+			//APPLY CLOCK. START THE TIMER
+			TCCR1B |= timer_clock;
+			break;
+
+		default:
+			break;
+	}
+}
+
+void AVR_TIMER_Enable_Mode_Ctc(uint8_t timer_num, uint8_t timer_clock)
+{
+	//INITIALIZE AND START THE TIMER IN CTC MODE WITH THE SPECIFIED
+	//CLOCK AND SPECIFIED TOP VALUE
+	
+	switch(timer_num)
+	{
+		case AVR_TIMER_8BIT_TIMER0:
+			//TIMER0 IN ATMEGA8 DOES NOT HAVE CTC MODE
+			return;
+			break;
+
+		case AVR_TIMER_8BIT_TIMER2:
+			//CLEAR MODE AND SET CTC MODE
+			TCCR2 &= ~((1 << WGM21) | (1 << WGM20));
+			TCCR2 |= (1 << WGM21);
+			//CLEAR COUNT
+			TCNT2 = 0x00;
+			//APPLY CLOCK. START THE TIMER
+			TCCR2 |= timer_clock;
+			break;
+			
+		case AVR_TIMER_16BIT_TIMER1:
+			//CLEAR MODE AND SET CTC MODE
+			TCCR1A &= ~((1 << WGM11) | (1 << WGM10));
+			TCCR1B |= (1 << WGM12);
+			//CLEAR COUNT
+			TCNT1 = 0x00;
+			//APPLY CLOCK. START THE TIMER
+			TCCR1B |= timer_clock;
+			break;
+
+		default:
+			break;
+	}
+}
+
+void AVR_TIMER_Set_Oca_parameters(uint8_t timer_num, uint8_t oc_mode, uint16_t top_value, uint8_t interrupt_enable)
+{
+	//SET THE CTC OC-A PARAMETERS FOR THE SPECIFIED TIMER
+	
+	switch(timer_num)
+	{
+		case AVR_TIMER_8BIT_TIMER0:
+			//TIMER0 IN ATMEGA8 DOES NOT HAVE CTC MODE
+			return;
+			break;
+
+		case AVR_TIMER_8BIT_TIMER2:
+			//SET THE OC-A MODE 
+			TCCR2 |= (oc_mode << 4);
+			//SET THE TOP VALUE
+			OCR2 = top_value;
+			if(interrupt_enable == AVR_TIMER_INTERRUPT_ON)
+			{
+				TIMSK |= (1 << OCIE2);
+			}
+			break;
+
+		case AVR_TIMER_16BIT_TIMER1:
+			//SET THE OC-A MODE 
+			TCCR1A |= (oc_mode << 6);
+			//SET THE TOP VALUE
+			OCR1A = top_value;
+			if(interrupt_enable == AVR_TIMER_INTERRUPT_ON)
+			{
+				TIMSK |= (1 << OCIE1A);
+			}
+			break;
+			
+		default:
+			break;
+	}
+}
+
+void AVR_TIMER_Set_Ocb_parameters(uint8_t timer_num, uint8_t oc_mode, uint16_t top_value, uint8_t interrupt_enable)
+{
+	//SET THE CTC OC-B PARAMETERS FOR THE SPECIFIED TIMER
+	
+	switch(timer_num)
+	{
+		case AVR_TIMER_8BIT_TIMER0:
+			//TIMER0 IN ATMEGA8 DOES NOT HAVE CTC MODE
+			return;
+			break;
+
+		case AVR_TIMER_8BIT_TIMER2:
+			//SET THE OC-B MODE 
+			//TIMER2 ONLY SUPPORTS OC-A MODE
+			return;
+			break;
+		
+		case AVR_TIMER_16BIT_TIMER1:
+			//SET THE OC-B MODE 
+			TCCR1A |= (oc_mode << 4);
+			//SET THE TOP VALUE
+			OCR1B = top_value;
+			if(interrupt_enable == AVR_TIMER_INTERRUPT_ON)
+			{
+				TIMSK |= (1 << OCIE1B);
+			}
+			break;
+				
+		default:
+			break;
+	}
+}
+
+uint8_t AVR_TIMER_Get_Flag_Value(uint8_t timer_num, uint8_t timer_flag)
+{
+	//RETURN THE SPECIFIED FLAG VALUE OF THE SPECIFIED TIMER
+	
+	switch(timer_num)
+	{
+		case AVR_TIMER_8BIT_TIMER0:
+			return (((TIFR & timer_flag) != 0)? 1 : 0);
+			break;
+		
+		case AVR_TIMER_8BIT_TIMER2:
+			return (((TIFR & timer_flag) != 0)? 1 : 0);
+			break;
+
+		case AVR_TIMER_16BIT_TIMER1:
+			return (((TIFR & timer_flag) != 0)? 1 : 0);
+			break;
+		
+		default:
+			break;
+	}
+	return 0;
+}
+
+void AVR_TIMER_Clear_Flag(uint8_t timer_num, uint8_t timer_flag)
+{
+	//CLEAR THE SPECIFIED FLAG OF THE SPECIFIED TIMER
+	//TO CLEAR THE TIMER FLAG, WE HAVE TO WRITE 1 TO THE
+	//FLAG POSITION
+
+	switch(timer_num)
+	{
+		case AVR_TIMER_8BIT_TIMER0:
+			TIFR |= timer_flag;
+			break;
+
+		case AVR_TIMER_8BIT_TIMER2:
+			TIFR |= timer_flag;
+			break;
+		
+		case AVR_TIMER_16BIT_TIMER1:
+			TIFR |= timer_flag;
+			break;
+
+		default:
+			break;
+	}
+
+}
+
+void AVR_TIMER_Disable(uint8_t timer_num)
+{
+	//DISABLE THE SPECIFIED TIMER AND RESET
+	//ITS COUNT
+	
+	switch(timer_num)
+	{
+		case AVR_TIMER_8BIT_TIMER0:
+			//STOP CLOCK TO TIMER
+			TCCR0 = 0x00;
+			//CLEAR COUNT
+			TCNT0 = 0;
+			//DISABLE INTERRUPT
+			TIMSK &= ~(1 << TOIE0);
+			break;
+		
+		case AVR_TIMER_8BIT_TIMER2:
+			//STOP CLOCK TO TIMER
+			TCCR2 = 0x00;
+			//CLEAR COUNT
+			TCNT2 = 0;
+			//DISABLE INTERRUPT
+			TIMSK &= ~((1 << TOIE2) | (1 << OCIE2));
+			break;
+		
+		case AVR_TIMER_16BIT_TIMER1:
+			//STOP CLOCK TO TIMER
+			TCCR1B = 0x00;
+			//CLEAR COUNT
+			TCNT1 = 0;
+			//DISABLE INTERRUPT
+			TIMSK &= ~((1 << TOIE2) | (1 << OCIE1A) | (1 << OCIE1B));
+			break;
+
+		default:
+			break;
+	}
+}
--- AVR_TIMER_ATMEGA8.h
+++ AVR_TIMER_ATMEGA8.h
@@ -23,6 +23,11 @@
 // *TIMER2 SAME AS TIMER0 EXCEPT IT SUPPORTS MORE
 // CLOCK PRESCALING OPTIONS
 //
+// MODES OF OPERATION
+//	1. TIMER 0 - 8 BIT - OVF
+//	2. TIMER 1 - 16 BIT - OVF, OC-A, OC-B [CTC MODE ON OC-A]
+//	3. TIMER 2 - 8 BIT - OVF, OC-A [CTC MODE ON OC-A]
+//
 // ONLY SUPPORTS THE FOLLOWING CLOCK SOURCES:
 //	1. INTERNAL (FROM IO CLOCK)
 //
@@ -45,12 +50,62 @@
 // ANKIT BHATNAGAR
 // ANKIT.BHATNAGARINDIA@GMAIL.COM
 ///////////////////////////////////////////////////////
-
 #ifndef _AVR_TIMER_ATMEGA8_H_
 #define _AVR_TIMER_ATMEGA8_H_
 
-
 #include <stdio.h>
 #include <avr/io.h>
+
+#define AVR_TIMER_8BIT_TIMER0	0
+#define AVR_TIMER_16BIT_TIMER1	1
+#define AVR_TIMER_8BIT_TIMER2	2
+
+#define AVR_TIMER_OPMODE_OC_NONE	0x00
+#define AVR_TIMER_OPMODE_OC_TOGGLE	0x01
+#define AVR_TIMER_OPMODE_OC_CLEAR	0x02
+#define AVR_TIMER_OPMODE_CTC_SET	0x03
+
+#define AVR_TIMER_TIM0_CLOCK_DISABLE		0x00
+#define AVR_TIMER_TIM0_CLOCK_PRESCALE_NONE	0x01
+#define AVR_TIMER_TIM0_CLOCK_PRESCALE_8		0x02
+#define AVR_TIMER_TIM0_CLOCK_PRESCALE_64	0x03
+#define AVR_TIMER_TIM0_CLOCK_PRESCALE_256	0x04
+#define AVR_TIMER_TIM0_CLOCK_PRESCALE_1024	0x05
+
+#define AVR_TIMER_TIM1_CLOCK_DISABLE		0x00
+#define AVR_TIMER_TIM1_CLOCK_PRESCALE_NONE	0x01
+#define AVR_TIMER_TIM1_CLOCK_PRESCALE_8		0x02
+#define AVR_TIMER_TIM1_CLOCK_PRESCALE_64	0x03
+#define AVR_TIMER_TIM1_CLOCK_PRESCALE_256	0x04
+#define AVR_TIMER_TIM1_CLOCK_PRESCALE_1024	0x05
+
+#define AVR_TIMER_TIM2_CLOCK_DISABLE		0x00
+#define AVR_TIMER_TIM2_CLOCK_PRESCALE_NONE	0x01
+#define AVR_TIMER_TIM2_CLOCK_PRESCALE_8		0x02
+#define AVR_TIMER_TIM2_CLOCK_PRESCALE_32	0x03
+#define AVR_TIMER_TIM2_CLOCK_PRESCALE_64	0x04
+#define AVR_TIMER_TIM2_CLOCK_PRESCALE_128	0x05
+#define AVR_TIMER_TIM2_CLOCK_PRESCALE_256	0x06
+#define AVR_TIMER_TIM2_CLOCK_PRESCALE_1024	0x07
+
+#define AVR_TIMER_INTERRUPT_OFF	0
+#define AVR_TIMER_INTERRUPT_ON	1
+
+#define AVR_TIMER_TIM0_FLAG_OVERFLOW	0x01
+#define AVR_TIMER_TIM1_FLAG_OVERFLOW	0x04
+#define AVR_TIMER_TIM1_FLAG_OCA_MATCH	0x10
+#define AVR_TIMER_TIM1_FLAG_OCB_MATCH	0x80
+#define AVR_TIMER_TIM2_FLAG_OVERFLOW	0x40
+#define AVR_TIMER_TIM2_FLAG_OCA_MATCH	0x80
+
+
+
+void AVR_TIMER_Enable_Mode_Normal(uint8_t timer_num, uint8_t timer_clock, uint8_t interrupt_enable);
+void AVR_TIMER_Enable_Mode_Ctc(uint8_t timer_num, uint8_t timer_clock);
+void AVR_TIMER_Set_Oca_parameters(uint8_t timer_num, uint8_t oc_mode, uint16_t top_value, uint8_t interrupt_enable);
+void AVR_TIMER_Set_Ocb_parameters(uint8_t timer_num, uint8_t oc_mode, uint16_t top_value, uint8_t interrupt_enable);
+uint8_t AVR_TIMER_Get_Flag_Value(uint8_t timer_num, uint8_t timer_flag);
+void AVR_TIMER_Clear_Flag(uint8_t timer_num, uint8_t timer_flag);
+void AVR_TIMER_Disable(uint8_t timer_num);
 
 #endif
